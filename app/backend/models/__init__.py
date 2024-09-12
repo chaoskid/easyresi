a95718960e0aea2580d984da9de25cbd10774d90
Alexander Blatchford <alexander.blatchford@gmail.com>
2024-09-12 15:13:00 +1000

    Added file structure front end, be sure to run the react app from frontend/ folder. -Alex

--- app/backend/models/__init__.py
+++
@@ -1,1 +0,0 @@
-# Optional for model initialisation
--- app/run.py
+++
@@ -1,8 +0,0 @@
-# Any entry point to run the Flask app
-
-from app import create_app
-
-app = create_app()
-
-if __name__ == '__main__':
-    app.run(debug=True)
