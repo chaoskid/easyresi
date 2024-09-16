7f6b5684b97ec2daacac7693e190e42b8437d1be
mafaraaz <sazmathullah2001@gmail.com>
2024-09-16 15:31:19 +1000

    Added questionnaire form and fixed visibility issues with radio buttons and checkboxes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom/client';  // For React 18+ use 'react-dom/client'
+import { ChakraProvider } from '@chakra-ui/react'; // Import ChakraProvider
+import App from './App';  // Import the main App component
+import './index.css';  // Import your global styles
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App /> {/* Your main application */}
+    </ChakraProvider>
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
